9164d8e70217b139a040f111b9ea4e4f4aae6294
Pulkit Kumar <pulkit@gogetspeedy.com>
2016-08-01 15:29:40 +0530

    fcm added

--- chat/src/main/java/com/joehukum/chat/messages/pubsub/JhGcmListenerService.java
+++ chat/src/main/java/com/joehukum/chat/messages/pubsub/JhGcmListenerService.java
@@ -2,24 +2,30 @@ package com.joehukum.chat.messages.pubsub;
 
 import android.os.Bundle;
 
-import com.google.android.gms.gcm.GcmListenerService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 import com.joehukum.chat.messages.sync.SyncUtils;
+
+import java.util.Map;
 
 /**
  * Created by pulkitkumar on 12/04/16.
  */
-public class JhGcmListenerService extends GcmListenerService
+public class JhGcmListenerService extends FirebaseMessagingService
 {
     private static final String JH_SENDER_ID = "JhSenderID";
 
     private static final String SENDER_ID = "senderId";
 
     @Override
-    public void onMessageReceived(String from, Bundle data)
+    public void onMessageReceived(RemoteMessage remoteMessage)
     {
-        if (data != null && !data.isEmpty())
+        super.onMessageReceived(remoteMessage);
+        String from = remoteMessage.getFrom();
+        Map data = remoteMessage.getData();
+        if (remoteMessage != null && !data.isEmpty())
         {
-            if (JH_SENDER_ID.equals(data.getString(SENDER_ID)))
+            if (JH_SENDER_ID.equals(data.get(SENDER_ID)))
             {
                 pullMessage();
             } else
@@ -30,6 +36,7 @@ public class JhGcmListenerService extends GcmListenerService
         {
             return;
         }
+
     }
 
     private void pullMessage()
